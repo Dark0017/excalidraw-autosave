442b32089d1b7ebb83f09f5f013580110ffef8df
Kshitiz Singh <k4droid3@gmail.com>
2021-07-12 19:21:38 +0530

    add basic excalidraw

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { useEffect, useRef } from "react";
+import Excalidraw, { exportToBlob } from "@excalidraw/excalidraw";
+
+import "./styles.scss";
+
 function App() {
-  return <h1>lmao</h1>;
+  const excalidrawRef = useRef(null);
+
+  useEffect(() => {
+    const onHashChange = () => {
+      const hash = new URLSearchParams(window.location.hash.slice(1));
+      const libraryUrl = hash.get("addLibrary");
+      if (libraryUrl) {
+        excalidrawRef.current.importLibrary(libraryUrl, hash.get("token"));
+      }
+    };
+    window.addEventListener("hashchange", onHashChange, false);
+    return () => {
+      window.removeEventListener("hashchange", onHashChange);
+    };
+  }, []);
+
+  return (
+    <div className="excalidraw-wrapper">
+      <Excalidraw ref={excalidrawRef} theme={"dark"} />
+    </div>
+  );
 }
 
 export default App;
